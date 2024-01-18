ab8edad2d858eb07a1894b2973df7f8042f12785
dhakaabhinay <dhakaabhinay26@gmail.com>
2024-01-18 16:06:46 +0530

    Add project files.

---
+++ LeaveManagment2.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagment2.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
